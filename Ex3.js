d03dda38938a962ba8ded38ab31d7be222c60676
Gabi Emi <gabiemi2021@gmail.com>
2025-04-11 11:12:51 -0300

    Exercício 3: Somando até 40

---
+++ Ex3.js
@@ -0,0 +1,10 @@
+let soma = 0;
+contador = 1;
+
+while (soma <= 40) {
+    soma += contador;
+    console.log (`Somei ${contador} que deu: ${soma}`);
+    contador++;
+}
+
+console.log (`A quantidade de números somados foi de ${contador}`);
